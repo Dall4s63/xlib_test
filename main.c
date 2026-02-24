f70e53242b97f08e787aa4254e294c84d6b169eb
Dylan Propert <dylanpropert63@gmail.com>
2026-02-24 12:59:54 +1100

    hehe

--- main.c
+++ main.c
@@ -9,6 +9,15 @@ int main(void)
     int s_no = DefaultScreen(d);
     // printf("default screen: %d\n", DefaultScreen(d));
 
+    int pmfv_count;
+    XPixmapFormatValues *pmfv = XListPixmapFormats(d, &pmfv_count);
+    for (int i = 0; i < pmfv_count; i++) {
+        printf("value: %d\n", i);
+        printf("    depth: \t\t%d\n", pmfv[i].depth);
+        printf("    bits per pixel: \t%d\n", pmfv[i].bits_per_pixel);
+        printf("    scanline pad: \t%d\n", pmfv[i].scanline_pad);
+    }
+
     printf("screen width: %u\n", DisplayWidth(d, s_no));
 
     Screen *s = DefaultScreenOfDisplay(d);
