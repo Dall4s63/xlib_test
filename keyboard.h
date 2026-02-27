4dbea06e3458048495bebd96b9abab0c2cbfde6f
Dylan Propert <dylanpropert63@gmail.com>
2026-02-27 22:12:31 +1100

    start the keyboard

---
+++ keyboard.h
@@ -0,0 +1,17 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+/* This file will hold generic preprocessor definitions for 
+ * keyboard keys for the game engine.
+ *
+ * This will be based on what I think is usefel at the time 
+ * I suppose.
+ */
+
+#ifdef KB_MISC
+#endif
+
+#ifdef KB_LATIN1
+#endif
+
+#endif
