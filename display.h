0eccce24bebaf021beb8eefdae7bbf58878a6ebb
Dylan <dylanpropert63@gmail.com>
2026-02-28 13:43:15 +1100

    ?????

--- display.h
+++ display.h
@@ -6,7 +6,7 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-// #include "keyboard.h"
+#include "keyboard.h"
 
 typedef struct _image {
     char *data;
@@ -18,8 +18,8 @@ typedef struct _event_callbacks {
     void (*button_press)(int button, int x, int y);
     void (*button_release)(int button, int x, int y);
 
-    // void (*key_press)(KeyId key, unsigned int scancode);
-    // void (*key_release)(KeyId key, unsigned int scancode);
+    void (*key_press)(KeyId key, unsigned int scancode);
+    void (*key_release)(KeyId key, unsigned int scancode);
 
     void (*window_resized)(int width, int height);
     void (*window_destroyed)(void);
