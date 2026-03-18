8081001dbf66e96914520361d6ab3fee9bb0f365
Dylan Propert <dylanpropert63@gmail.com>
2026-03-18 16:34:19 +1100

    make the hash code

--- hash_str_int.c
+++ hash_str_int.c
@@ -1,29 +1,126 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "hash_str_int.h"
 
-#define HASH_INIT_LEN 60
+#define HASH_BUF_LEN 60
 #define HASH_PRIME 67
 
 STOIHashMap stoi_hash_new(void) {
     STOIHashMap out;
-    out.values = malloc(sizeof(STOIHashUnit) * HASH_INIT_LEN);
+    out.values = malloc(sizeof(STOIHashUnit) * HASH_BUF_LEN);
     if (out.values == NULL) {
         // TODO we are in trouble
         printf("failed allocation\n");
     }
-    memset(out.values, 0, sizeof(STOIHashUnit) * HASH_INIT_LEN);
+    memset(out.values, 0, sizeof(STOIHashUnit) * HASH_BUF_LEN);
     return out;
 }
 
+static void free_unit(STOIHashUnit *u) {
+    if (u == NULL) {
+        return;
+    }
+    free_unit(u->next);
+    free(u->string);
+    free(u);
+}
+
 void stoi_hash_free(STOIHashMap m) {
+    for (int i = 0; i < HASH_BUF_LEN; ++i) {
+        free_unit(m.values[i].next);
+    }
+    free(m.values);
+}
+
+static int calc_hash(char *s) {
+    int v = 0;
+    int mul = 1;
+    while (*s++ != '\0') {
+        v += (int)*s * mul;
+        mul *= HASH_PRIME;
+    }
+    return v % HASH_BUF_LEN;
 }
 
 bool stoi_hash_get(STOIHashMap m, char *s, int *ret) {
+    int i = calc_hash(s);
+    if (m.values[i].string == NULL) {
+        return false;
+    }
+    if (strcmp(m.values[i].string, s) == 0) {
+        *ret = m.values[i].v;
+        return true;
+    }
+    STOIHashUnit *cur = m.values[i].next;
+    while (cur != NULL) {
+        if (strcmp(cur->string, s) == 0) {
+            *ret = cur->v;
+            return true;
+        }
+        cur = cur->next;
+    }
+    return false;
 }
 
 bool stoi_hash_put(STOIHashMap m, char *s, int v) {
+    int i = calc_hash(s);
+    if (m.values[i].string == NULL) {
+        int len = strlen(s) + 1;
+        m.values[i].string = malloc(sizeof(char) * len);
+        strcpy(m.values[i].string, s);
+        m.values[i].v = v;
+        m.values[i].next = NULL;
+        return false;
+    }
+    if (strcmp(m.values[i].string, s) == 0) {
+        m.values[i].v = v;
+        return true;
+    }
+    STOIHashUnit *prev = &(m.values[i]);
+    STOIHashUnit *cur = m.values[i].next;
+    while (cur != NULL) {
+        if (strcmp(cur->string, s) == 0) {
+            cur->v = v;
+            return true;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+    STOIHashUnit *new = malloc(sizeof(STOIHashUnit));
+    prev->next = new;
+    new->string = s;
+    new->v = v;
+    new->next = NULL;
+    return false;
 }
 
-#undef HASH_INIT_LEN
+bool stoi_hash_del(STOIHashMap m, char *s) {
+    int i = calc_hash(s);
+    if (m.values[i].string == NULL) {
+        return false;
+    }
+    if (strcmp(m.values[i].string, s) == 0) {
+        free(m.values[i].string);
+        m.values[i].string = NULL;
+        return true;
+    }
+    STOIHashUnit *prev = &(m.values[i]);
+    STOIHashUnit *cur = m.values[i].next;
+    while (cur != NULL) {
+        if (strcmp(cur->string, s) == 0) {
+            free(cur->string);
+            prev->next = cur->next;
+            free(cur);
+            return true;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+    return false;
+}
+
+#undef HASH_BUF_LEN
+#undef HASH_PRIME
 
