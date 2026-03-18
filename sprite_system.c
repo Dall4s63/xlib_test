80d5707302d49f3d96ff5fc71f58c45def4ea557
Dylan <dylanpropert63@gmail.com>
2026-03-18 18:01:38 +1100

    start a bit on the sprite system

--- sprite_system.c
+++ sprite_system.c
@@ -1,5 +1,7 @@
 #include "sprite_system.h"
 
+#define INIT_LEN 60
+
 typedef struct _sprite {
     unsigned char *data;
     int data_width;
@@ -18,21 +20,37 @@ static int     sprites_id_to_i_len = 0;
 static int    *sprites_i_to_id = NULL;
 static int     sprites_i_to_id_blen = 0;
 static int     sprites_i_to_id_len = 0;
+static int    *sprites_id_to_ref = NULL;
+static int     sprites_id_to_ref_blen = 0;
+static int     sprites_id_to_ref_len = 0;
+
+static STOIHashMap name_map;
 
 static void sprite_initialize() {
+    name_map = stoi_hash_new();
+    sprites = malloc(sizeof(Sprite) * INIT_LEN);
+    sprites_blen = INIT_LEN;
+    sprites_id_to_i = malloc(sizeof(int) * INIT_LEN);
+    sprites_id_to_i_blen = INIT_LEN;
+    sprites_i_to_id = malloc(sizeof(int) * INIT_LEN);
+    sprites_i_to_id_blen = INIT_LEN;
+    sprites_id_to_ref = malloc(sizeof(int) * INIT_LEN);
+    sprites_id_to_ref_blen = INIT_LEN;
 }
 
 int sprite_new(char *filename) {
     if (sprites == NULL) {
         sprite_initialize();
     }
-    // 1. check if there is already a sprite resource 
-    // associated with this filename, and if there is 
-    // just return the associated id and increment the 
-    // ref-count.
-    //
-    // 2. store the string and create a new sprite 
-    // resource 
+    int id;
+    bool res = stoi_hash_get(name_map, filename, &id);
+    if (res) {
+        sprites_id_to_ref[id] += 1;
+        return id;
+    }
+    int index = sprites_len;
+    // TODO expand the buffers
+    // load the image
 }
 
 void sprite_free(int id) {
@@ -46,4 +64,6 @@ int sprite_height(int id) {
 
 FColor sprite_sample(int id, int x, int y) {
 }
+
+#undef INIT_LEN
 
